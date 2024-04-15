faa44dd6f86e368b67bdb1004b639f3d88770ed9
Sibindra <timalsinasibindra@gmail.com>
2024-04-15 20:22:25 +0545

    feat: react query setup

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,16 @@ import ReactDOM from "react-dom/client";
 import "@/assets/styles/index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "@/config/Routes.tsx";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </React.StrictMode>
 );
