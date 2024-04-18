5f1e17778a4514d35fae27da75ff1d030754d04d
Sibindra <timalsinasibindra@gmail.com>
2024-04-18 18:29:33 +0545

    feat: minor changes at app

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
-const App = () => <div>this is app</div>;
+import { Link } from "react-router-dom";
+
+const App = () => (
+  <div>
+    this is app <br />{" "}
+    <Link to={"/login"} className=" underline ">
+      login here
+    </Link>{" "}
+    <br />{" "}
+    <Link to={"register"} className=" underline">
+      register here
+    </Link>
+  </div>
+);
 
 export default App;
