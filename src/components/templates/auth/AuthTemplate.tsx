a4d6dcf8eae30c240e2eb861958e140a3c4478b4
Sibindra <timalsinasibindra@gmail.com>
2024-04-21 07:24:09 +0545

    fix: remove img from auth template

--- src/components/templates/auth/AuthTemplate.tsx
+++ src/components/templates/auth/AuthTemplate.tsx
@@ -7,17 +7,7 @@ interface AuthTemplateProps extends TemplateProps {
 export default function AuthTemplate({ children }: AuthTemplateProps) {
   return (
     <div className="h-screen overflow-hidden flex justify-center items-center border">
-      <div className="flex justify-center gap-5 items-center ">
-        <div className="mt-10 md:w-1/2 p-10 flex flex-col gap-10">{children}</div>
-        <div className=" hidden md:block md:w-1/2 px-16  py-10 ">
-          <img
-            loading="lazy"
-            src="https://images.unsplash.com/photo-1579783902614-a3fb3927b6a5?q=80&w=3145&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Welcome Image"
-            className="object-cover w-full h-full rounded-2xl"
-          />
-        </div>
-      </div>
+      <div className=" p-10 flex flex-col gap-10 border rounded-xl">{children}</div>
     </div>
   );
 }
